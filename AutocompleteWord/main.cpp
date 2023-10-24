dd57bc83f56c8dd46ed8dc7c1d0c72ea12b316b7
Azad <Azad.hesso90@gmail.com>
2023-10-24 03:45:15 +0300

    Show dictionary of English words

--- AutocompleteWord/main.cpp
+++ AutocompleteWord/main.cpp
@@ -3,6 +3,27 @@
 
 int main() {
 
+	setlocale(LC_ALL, "");
+	TrieNode* root = getNewTrieNode();
 
+	addWord(root, "sky");
+	addWord(root, "tree");
+	addWord(root, "window");
+	addWord(root, "sound");
+	addWord(root, "mount");
+	addWord(root, "flag");
+	addWord(root, "install");
+	addWord(root, "shutdown");
+	addWord(root, "search");
+	addWord(root, "find");
+	addWord(root, "war");
+	addWord(root, "drive");
+
+	std::vector<std::string> words;
+	std::cout << "Словарь Английских слов: \n";
+	getAllWords(root, "", words);
+	for (const auto& word : words) {
+		std::cout << "\t" << word << " " << "\n";
+	   }
 
 }
